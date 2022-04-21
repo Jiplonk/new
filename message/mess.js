978a1e2ccf02f81b869812d7815d44ef8944efba
KazuKi <100437797+Jiplonk@users.noreply.github.com>
2022-04-21 20:05:31 +0700

    Add files via upload

--- message/mess.js
+++ message/mess.js
@@ -1,10 +1,10 @@
 module.exports.mess = {
-				regist: '─ 「 *NOT REGISTERED* 」 ─\n\n*ENGLISH*\n_YOU HAVE NOT REGISTERED ON THE BOTTOM OF I DECK, TYPE #verify FOR RUQYAH_\n\n*INDONESIA*\n_LUH BELUM DAFTAR DI BOT GUA DEX, KETIK #daftar UNTUK DI RUQYAH_',
+				regist: '─ 「 *NOT REGISTERED* 」 ─\n\n*ENGLISH*\n_YOU HAVE NOT REGISTERED ON THE BOTTOM OF I DECK, TYPE #verify FOR CAN USE KAZUKI BOTS_\n\n*INDONESIA*\n_LUH BELUM DAFTAR DI BOT GUA DEX, KETIK #daftar UNTUK BISA MENGGUNAKAN BOT KAZUKI_',
 				dewasa: '─ 「 *WARNING* 」 ─\n\n*ENGLISH*\n_not allowed for minors, if you are an adult, please verify first, type #my18_\n\n*INDONESIA*\n_dilarang untuk anak di bawah umur, jika Anda sudah dewasa, silahkan verifikasi terlebih dahulu, ketik #saya18_',
 				kecil: '_luh masih kecil dek, mending main epep_',
 				wait: '_wait deck..._',
 				banned: '_you have been blocked, contact the owner for ruqyah_',
-				success: '_succeed, dont forget to subscribe_ : https://youtube.com/c/RamdaniOfficial',
+				success: '_succeed, dont forget to subscribe_ : https://youtube.com/c/KazuKi',
 				error: {
 		        apikey: '_error, apikey is not defined:(_',
 		        format: '_format error_',
@@ -12,12 +12,12 @@ module.exports.mess = {
 			    link: '*English*\n_what link did you send goblog: v_\n\n*indonesian*\n_Luh ngasih link apaan goblog:v_'
 				},
 				only: {
-					group: '*THIS FEATURE CAN ONLY BE USED FOR GROUPS!!!*',
-					premium: '*YOU IS NOT A PREMIUM USER, CHAT OWNER TO RUQYAH!!!*',
-					ownerG: '*THIS FEATURE CAN ONLY BE USED BY THE OWNER GROUP!!!*',
-					bot: '*THIS FEATURE CAN ONLY BE USED BY THE NUMBER BOT!!!*',
-					ownerB: '*THIS FEATURE CAN ONLY BE USED BY THE OWNER BOT!!!*',
-					admin: '*THIS FEATURE CAN ONLY BE USED BY GROUP ADMINS!!! ️*',
-					Badmin: '*THIS FEATURE CAN ONLY BE USED WHEN THE BOTS BECOME ADMIN!!!*'
+					group: '*FITUR INI HANYA BISA DIGUNAKAN UNTUK GRUP!!!*',
+                    premium: '*ANDA BUKAN PENGGUNA PREMIUM, CHAT OWNER KE RUQYAH!!!*',
+                    ownerG: '*FITUR INI HANYA BISA DIGUNAKAN OLEH OWNER GROUP!!!*',
+                    bot: '*FITUR INI HANYA BISA DIGUNAKAN OLEH NOMOR BOT!!!*',
+                    ownerB: '*FITUR INI HANYA BISA DIGUNAKAN OLEH PEMILIK BOT!!!*',
+                    admin: '*FITUR INI HANYA BISA DIGUNAKAN OLEH ADMIN GROUP!!! ️*',
+					Badmin: 'FITUR INI HANYA BISA DIGUNAKAN JIKA BOT MENJADI ADMIN!!!'
 				}
 			}
